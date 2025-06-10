c0b16ea6d505abf0ae1f7b0dcf8ba4fe8db03954
MBANS8A1 <seanmbng@aol.com>
2025-06-10 11:52:44 +0100

    Converted the constraint layout in activity_main.xml to FrameLayout and changed the id. In HomeFragment.kt I implemented the onViewCreated() function, which contains a click listener on the Next button that will eventually pass data via a bundle to the QuestionFragment. Validation is present too.

--- app/src/main/java/com/example/quizapp/HomeFragment.kt
+++ app/src/main/java/com/example/quizapp/HomeFragment.kt
@@ -5,35 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.quizapp.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var bundle: Bundle
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btn.setOnClickListener {
+            val fragment = QuestionFragment()
+            bundle.apply {
+                putString("name",binding.nameET.text.toString())
+            }
+            when{
+                binding.nameET.text.toString().isEmpty()->{
+                    binding.nameET.error="Enter you name first!"
+                }
+                else->{
+                    fragment.arguments = bundle
+                    requireActivity()
                 }
             }
+        }
     }
+
 }
